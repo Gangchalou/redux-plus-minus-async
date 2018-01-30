e8dafc4720bb2bae81c90c7d75e87d73c8fbead8
WenShuZhou <shuzhou468@sina.com>
2018-01-30 18:58:35 +0800

    update

--- src-counter1_react/index.js
+++ src-counter1_react/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
 
 import App from './components/app'
-import {counter} from './redux/reducers'
 
-//生成一个store对象
-const store = createStore(counter)
 ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ src/redux/reducers.js
@@ -0,0 +1,14 @@
+/*包含n个reducer的函数的模块*/
+import {INCREMENT,DECREMENT} from './action-types'
+
+export function counter(state = 0,action) {
+    console.log('conunter()',state,action);
+    switch (action.type){
+        case INCREMENT:
+            return state + action.data
+        case DECREMENT:
+            return state - action.data
+        default:
+            return state
+    }
+}
